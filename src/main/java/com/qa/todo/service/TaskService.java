80a9d2d3e9b355fd7b72575bc519dfda18a8ee5e
Chris <christopher.perrins@qa.com>
2019-01-04 10:06:37 +0000

    fixed delete issue

--- src/main/java/com/qa/todo/service/TaskService.java
+++ src/main/java/com/qa/todo/service/TaskService.java
@@ -23,7 +23,6 @@ public class TaskService {
 	private UserTasksRepository userTasksRepository;
 	
 	public Iterable<Task> getTasks(String apikey) {
-		System.out.println("get1");
 		
 		ArrayList<Task> tasks = new ArrayList<Task>();
 		ArrayList<String> taskIds = new ArrayList<String>();
@@ -33,43 +32,31 @@ public class TaskService {
 				taskIds.add(userTasks.getUuid());
 			}
 		}
-		System.out.println("get2");
 		
 		for(String taskId : taskIds) {
 			tasks.add(taskRepository.findById(taskId).get());
 		}		
-		System.out.println("get3");
 		
 		return tasks;
 	}
 	
 	public Iterable<Task> postTask(@RequestBody Task task, String apikey) {
-		System.out.println("post1");
 		if (taskRepository.existsById(task.getIdentifier())) {
 			return null;
 		}	
-		System.out.println("post2");
 		UserTasks userTasks = new UserTasks(apikey, task.getIdentifier());
-		System.out.println("post3");
 		userTasksRepository.save(userTasks);
-		System.out.println("post4");
 		taskRepository.save(task);
-		System.out.println("post5");
 		return getTasks(apikey);
 	}
 	
 	public Iterable<Task> putTask(@RequestBody Task newTask, @PathVariable String identifier, String apikey) {
-		System.out.println("put1");
 		if(!userTasksRepository.findById(identifier).get().getAuthToken().equals(apikey)) {
 			return null;
 		}
-		System.out.println("put2");
 		Task task = taskRepository.findById(identifier).get();
-		System.out.println("put3");
 		task.setText(newTask.getText());
-		System.out.println("put4");
 		taskRepository.save(task);
-		System.out.println("put5");
 		return getTasks(apikey);
 	}
 	
@@ -80,9 +67,14 @@ public class TaskService {
 		}
 		System.out.println("delete2");
 		Task task = taskRepository.findById(identifier).get();
-		System.out.println("delete3");
+		String taskID = task.getIdentifier();
+		Iterable<UserTasks> allUserTasks = userTasksRepository.findAll();
+		for (UserTasks userTasks : allUserTasks) {
+			if(userTasks.getUuid().equals(taskID)) {
+				userTasksRepository.delete(userTasks);
+			}
+		}
 		taskRepository.delete(task);
-		System.out.println("delete4");
 		return getTasks(apikey);
 	}
 }
